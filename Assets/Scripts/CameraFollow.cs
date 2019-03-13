efd20b1e60bfa76312ceac62a98ec2b39a728cb8
Genís <genisenco@gmail.com>
2019-03-13 18:36:42 +0100

    cames alternades i intent arreglar camera

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -20,16 +20,21 @@ public class CameraFollow : MonoBehaviour
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player");
-        
+        transform.position = new Vector3(player.transform.position.x, player.transform.position.y, transform.position.z);
+
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        float posX = Mathf.SmoothDamp(transform.position.x, player.transform.position.x, ref velocity.x, smoothTimeX);
-        float posY = Mathf.SmoothDamp(transform.position.y, player.transform.position.y, ref velocity.y, smoothTimeY);
+        //float posX = Mathf.SmoothDamp(transform.position.x, player.transform.position.x, ref velocity.x, smoothTimeX);
+        //float posY = Mathf.SmoothDamp(transform.position.y, player.transform.position.y, ref velocity.y, smoothTimeY);
+        
 
-        transform.position = new Vector3(posX, posY, transform.position.z);
+        //transform.position = new Vector3(posX, posY, transform.position.z);
+        transform.position = new Vector3(player.transform.position.x, player.transform.position.y, transform.position.z);
+        Debug.Log("player: " + player.transform.position);
+        Debug.Log("Camera: " + transform.position);
 
         if (bounds)
         {
